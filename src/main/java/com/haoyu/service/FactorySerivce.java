55169201840c83e391b5b907e3735fdb6f2b018f
Administrator <Administrator@USER-20180827VT>
2019-06-28 10:10:40 +0800

    产品作废入库逻辑更新

--- src/main/java/com/haoyu/service/FactorySerivce.java
+++ src/main/java/com/haoyu/service/FactorySerivce.java
@@ -240,5 +240,49 @@ public class FactorySerivce {
 		}
 		return null;
 	}
-	
+	//产品作废处理
+	public void stepCheckWaste(String id, String storageid2, String chistoryStep, String checkgoal, String checkresult,
+			String chistoryRemark) {
+		if(StringUtils.isNotBlank(checkresult)) {
+			//通过环节
+			//insert
+			if(checkresult.equals("产品作废")) {
+				MesChistory mc=MesChistory.builder().chistoryFactoryid(Integer.parseInt(id))//
+						.chistoryStep(chistoryStep).chistoryTime(new Date())//
+						.chistoryGoal(checkgoal).chistoryResult(checkresult).chistoryRemark(chistoryRemark).chistoryStatus(1).build();
+				//factory-prostatus
+				MesFactory factory=mesFactoryMapper.selectByPrimaryKey(Integer.parseInt(id));
+				factory.setFactoryProstatus("生产结束");
+				factory.setFactoryStatus(0);//结束生产可查状态
+				Integer pid=factory.getFactoryProductid();
+				//////////////////////////////////////////////////////////////////////
+				MesProduct product=mesProductMapper.selectByPrimaryKey(pid);
+				product.setProductMaterialsource("废料");
+				//新增库存操作
+				MesStock mesStock=MesStock.builder().stockProductid(pid).stockProductname(product.getProductMaterialname())//
+						.stockImgid(product.getProductImgid()).stockProductsource(product.getProductMaterialsource())//
+						.stockStatus(1)//
+						.stockStorageid(Integer.parseInt(storageid2)).build();
+				//Order*
+				if(product!=null) {
+					Integer orderid=product.getProductOrderid();
+					MesOrder mesOrder=mesOrderMapper.selectByPrimaryKey(orderid);
+					if(mesOrder!=null) {
+						mesStock.setStockOrderid(orderid);
+						mesStock.setStockOrdername(mesOrder.getOrderProductname());
+					}
+					mesStock.setStockStoragestatus(1);//待入库
+				}
+				//更新材料来源属性
+				mesProductMapper.updateByPrimaryKeySelective(product);
+				//生产环节记录更新
+				mesChistoryMapper.insertSelective(mc);
+				//增加一条库存记录-待入库
+				mesStockMapper.insertSelective(mesStock);
+				//////////////////////////////////////////////////////////////////////
+				//生产状态结束
+				mesFactoryMapper.updateByPrimaryKeySelective(factory);
+			}
+		}
+	}
 }
